71520fa3e88e112f236644158a035de2bc17ca93
JAlexander94 <118635922+JAlexander94@users.noreply.github.com>
2023-01-10 18:59:24 +0000

    first challenge done

---
+++ activities/02-Stu-Objects/starter/script.js
@@ -0,0 +1,15 @@
+//WRITE YOUR CODE BELOW
+let customerOrder = {
+    personName: "Jenny",
+    drinkName: "Flat White",
+    numbersugar: 2,
+    orderReady: true
+}
+
+console.log(customerOrder.drinkName,customerOrder.numbersugar)
+
+if (customerOrder.orderReady=true) {
+    alert("Ready for pick-up")
+} else {
+    alert("Still in order queue")
+}
